93117bb2c9372edea8c516bea1505ff510786826
Chek Yin Choi <chekchek2007@gmail.com>
2020-11-26 11:27:07 +0100

    forgot what are these

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import Idx from './index'
 
+//Render HomeButton
+class HB extends React.Component {
+    render () {
+	return(
+		<div className="logo"> <img src="./hb.png" width="50" height="50"></img></div>
+	);
+    }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-	 <Idx />
+	    <div className="App">
+	    <noscript>Please enable JavaScript in your browser!</noscript>
+	    	   <div className="hb"><HB /></div>
+            <header className="App-header">
+
+	 <Idx id="idx"/>
 
         </header>
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Animate, Transition } from 'react-move';
 import './index.css';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+
+
 
 // Render Blocks
 
@@ -12,25 +15,13 @@ class Blk extends React.Component {
 	    super(props);
 	    this.state = {
 		value: this.props.value,
+		des: null
 	    };
-	}
-    render () {
-	
-	return (
-		<button className="blk" onClick={() => this.setState({value: 'X'})}>
-		    {this.state.value}
-	</button>
-	);
     }
-}
 
-//Render Bottom Block
-
-class BtmBlk extends React.Component {
-    render () {
-	return (<button className="btmblk">
-		Email / Github
-		</button>);
+    handleClick(i className="logo"> <img src="./gh.png" width="50" height="50"></img></a>
+		<a href="mailto:chekchek2007@gmail.com">Email</a>
+		</div>);
     }
 }
 
@@ -40,30 +31,50 @@ class Bg extends React.Component {
      constructor(props) {
     super(props);
     this.state = {
-      blks: Array(8).fill(null),
+	blks: Array(6).fill("v"),
+	clicked: false
     };
-  }
-    renderBlk(i) {
-	return <Blk value={i} />;
+     }
+
+    invisible(e){
+	e = <div style="visibility: hidden">  </div>;
+	return e;
+    }
+    handleClick(i){
+	const blks = this.state.blks.slice();
+	for (var b in blks){
+	    
+	}
+	this.state.clicked = true
+	const element = this.invisible(document.getElementsByClassName('blk')[0]);
+	const clickedBlk = document.getElementsByClassName('blk')[i];
+	ReactDOM.hydrate(element, document.getElementsByClassName('idxPage')[0]);
+
+    }
+
+    renderBlk(i, j) {
+	return <Blk value={i} row={j} onClick={() => this.handleClick(i)} />;
+
     }
     renderBtm(i){
 	return <BtmBlk value={i} />;
     }
-     render() {
-    const status = 'Next player: X';
-    return (
+
+    render() {
+	 return (
+	    
       <div>
         <div className="row">
-          {this.renderBlk("Chek")}
-          {this.renderBlk("About Me")}
+         {this.renderBlk(0, 0)}
+        {this.renderBlk(1, 0)}
+    </div>
+        <div className="row">
+            {this.renderBlk(2,1)}
+        {this.renderBlk(3,1)}
         </div>
         <div className="row">
-          {this.renderBlk(2)}
-          {this.renderBlk(3)}
-        </div>
-        <div className="row">
-          {this.renderBlk(4)}
-          {this.renderBlk(5)}
+            {this.renderBlk(4,2)}
+        {this.renderBlk(5,2)}
         </div>
 	<div className="row">
           {this.renderBtm(6)}
@@ -74,9 +85,16 @@ class Bg extends React.Component {
 }
 
 class Idx extends React.Component {
+         constructor(props) {
+    super(props);
+    this.state = {
+	obj: 'bg'
+    };
+     }
   render() {
       return (
 	      <div className="idxPage">
+
 	       
         <div className="Bg">
           <Bg />
